1aed8b7ceffbd9b605d2fc6ff1bc3152cc540279
nandan00n <nandankumar00n@gmail.com>
2022-05-24 23:14:37 +0530

    fixing

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function Cards({data}) {
@@ -16,7 +14,7 @@ export default function Cards({data}) {
   return (
     <>
     <Card sx={{ maxWidth: 345, marginTop:"10vh" }}>
-      <a href={"https://www.youtube.com/watch?v=" + id.videoId} target="_blank">
+      <a href={"https://www.youtube.com/watch?v=" + id.videoId} rel="noreferrer" target="_blank">
       <CardMedia
         component="img"
         height="140"
